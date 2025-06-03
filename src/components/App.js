48c5e1585d11feebd1dd637c89c27e0574440261
ArtRosh <artem.rosh17@gmail.com>
2025-06-03 13:59:29 -0700

    lab is done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,43 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+import { data } from "../mocks/data";
+// import { handlers } from "../mocks/handlers";
+// import { server } from "../mocks/server";
+// import { worker } from "../mocks/worker";
+
+
+function App() {
+
+    const[dog, setDog] = useState(null)
+
+    const[isLoading, setIsLoading] = useState(true)
+
+  
+    
+    useEffect(() => {
+        setIsLoading(true)
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => {
+                console.log(res)
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setDog(data.message)
+                setIsLoading(false)
+            })
+       
+    }, [])
+
+    return(
+        <div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <img src={dog} alt="A Random Dog" />
+            )}
+        </div>
+    )
+}
+
+export default App;
